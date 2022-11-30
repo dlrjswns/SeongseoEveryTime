44c5c4660eaa6049d2cb54dc700ac91c78fe90ca
dlrjswns <leekyunjun@gmail.com>
2022-11-30 22:54:44 +0900

    authRouter 추가

--- app.js
+++ app.js
@@ -1,74 +1,77 @@
-const path = require('path');
+const path = require("path");
 
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-const nunjucks = require('nunjucks');
-const { sequelize } = require('./models');
+const nunjucks = require("nunjucks");
+const { sequelize } = require("./models");
 
-const userRouter = require('./routes/user');
-const commentRouter = require('./routes/comment');
-const followRouter = require('./routes/follow');
-const likeRouter = require('./routes/like');
-const postingRouter = require('./routes/posting');
-const indexRouter = require('./routes');
-
+const userRouter = require("./routes/user");
+const commentRouter = require("./routes/comment");
+const followRouter = require("./routes/follow");
+const likeRouter = require("./routes/like");
+const postingRouter = require("./routes/posting");
+const authRouter = require("./routes/auth");
+const indexRouter = require("./routes");
 
 dotenv.config();
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.set('view engine', 'html');
-nunjucks.configure(path.join(__dirname, 'views'), {
-    express: app,
-    watch: true,
+app.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, "views"), {
+  express: app,
+  watch: true,
 });
 
-sequelize.sync({ force: false })
-  .then(() => console.log('데이터베이스 연결 성공'))
-  .catch(err => console.error(err));
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("데이터베이스 연결 성공"))
+  .catch((err) => console.error(err));
 
 app.use(
-    morgan('dev'),
-    express.static(path.join(__dirname, 'public')),
-    express.json(),
-    express.urlencoded({ extended: false }),
-    cookieParser(process.env.SECRET),
-    session({
-        resave: false,
-        saveUninitialized: false,
-        secret: process.env.SECRET,
-        cookie: {
-            httpOnly: true,
-            secure: false
-        },
-        name: 'session-cookie'
-    })
+  morgan("dev"),
+  express.static(path.join(__dirname, "public")),
+  express.json(),
+  express.urlencoded({ extended: false }),
+  cookieParser(process.env.SECRET),
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
 );
 
-app.use('/user', userRouter);
-app.use('/comment', commentRouter);
-app.use('/like', likeRouter);
-app.use('/follow', followRouter);
-app.use('/posting', postingRouter);
-app.use('/', indexRouter);
+app.use("/user", userRouter);
+app.use("/comment", commentRouter);
+app.use("/like", likeRouter);
+app.use("/follow", followRouter);
+app.use("/posting", postingRouter);
+app.use("/auth", authRouter);
+app.use("/", indexRouter);
 
 app.use((req, res) =>
-    res.render('index', {
-        title: require('./package.json').name,
-        port: app.get('port')
-    }));
-    
+  res.render("index", {
+    title: require("./package.json").name,
+    port: app.get("port"),
+  })
+);
+
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err);
+  console.error(err);
+  res.status(500).send(err);
 });
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
 });
