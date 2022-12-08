757567335d9ec6dfbc2226417ff0a5a76085f8f3
dlrjswns <leekyunjun@gmail.com>
2022-12-08 15:23:30 +0900

    posting router 추가

--- routes/posting.js
+++ routes/posting.js
@@ -1,25 +1,66 @@
-const express = require('express');
-const Posting = require('../models/posting');
+const express = require("express");
+const Posting = require("../models/posting");
 
 const router = express.Router();
 
+router
+  .route("/:id/comments")
+  .get(async (req, res, next) => {
+    // 특정 게시글에 존재하는 comment들을 가져옵니다
+    const posting_id = req.params.id;
+    try {
+      const posting = await Posting.findOne({ where: { id: posting_id } });
+      const comments = posting.getComments();
+      res.json(comments);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    // 특정 게시글에 comment들을 등록합니다
+    const { posting_id, content } = req.body;
+    try {
+      const comment = await Comment.create({
+        posting_id,
+        content,
+      });
+      res.json(comment);
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
 
-router.route('/')
-    .get((req, res) => {
-        res.render('posting', {
-            title: require('../package.json').name
-        });
-    })
-    .post(async (req, res, next) => {
-        const { userId, comment } = req.body;
-
-        try {
-            await Posting.create({ userId, comment });
-            res.redirect('/');
-        } catch (err) {
-            console.error(err);
-            next(err);
-        }
-    });
+  router
+  .route("/:posting_id/comments/")
+  .get(async (req, res, next) => {
+    // 특정 게시글에 존재하는 comment들을 가져옵니다
+    const posting_id = req.params.id;
+    try {
+      const posting = await Posting.findOne({ where: { id: posting_id } });
+      const comments = posting.getComments();
+      res.json(comments);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    // 특정 게시글에 comment들을 등록합니다
+    const { posting_id, content } = req.body;
+    try {
+      const comment = await Comment.create({
+        posting_id,
+        content,
+      });
+      res.json(comment);
+      res.redirect("/");
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
 
 module.exports = router;
